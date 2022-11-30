a5ce2b85cef33f5a031119ea45934d78f0e81a16
junyeong.jo <whwnsdud2024@naver.com>
2022-11-30 11:39:14 +0900

    create backend server at 2022.11.30

---
+++ backend_server/src/main/java/teamJYNK/movieCritics/MovieCriticsApplication.java
@@ -0,0 +1,13 @@
+package teamJYNK.movieCritics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCriticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCriticsApplication.class, args);
+	}
+
+}
---
+++ backend_server/src/test/java/teamJYNK/movieCritics/MovieCriticsApplicationTests.java
@@ -0,0 +1,13 @@
+package teamJYNK.movieCritics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCriticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
